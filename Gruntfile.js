ac0fa59b59c4573bc5aabbe91868f33e04a7c63e
chris <christopher.hawkins@cohaesus.co.uk>
2016-01-16 14:23:30 +0000

    optimised images

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,7 +38,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,                  // Enable dynamic expansion 
           cwd: 'images/',                // Src matches are relative to this path 
-          src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match 
+          src: ['images/*.{png,jpg,gif}'],   // Actual patterns to match 
           dest: 'optimised/'        // Destination path prefix 
         }]
       },
